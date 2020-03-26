e5191363e8506af46432de12e154b99300a8b699
Andrew Steward <andrewsteward@quickenloans.com>
2020-03-25 22:43:42 -0400

    LAB 11.2 Coffee Shop

---
+++ LAB_11.2_CoffeeShop/LAB_11.2_CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB_11._2_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("RegistrationIndex");
+        }
+
+        [HttpPost]
+        public IActionResult RegistrationConfirmation(string fname, string email)
+        {
+            ViewData["name"] = fname;
+            ViewData["email"] = email;
+            return View();
+        }
+    }
+}
